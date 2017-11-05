5900505e382fdb37a810389112ea74fe6c4f8876
Rone <scyntherex@Rones-MacBook-Pro.local>
2017-11-05 15:37:26 -0800

    Implemented Mines.java

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -1,10 +1,41 @@
 package mines;
 
-public class Mines {
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class Mines extends JFrame {
+	
+	private final int FRAME_WIDTH = 250;
+    private final int FRAME_HEIGHT = 290;
+
+    private final JLabel statusbar;
+    
+    public Mines() {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        setLocationRelativeTo(null);
+        setTitle("Minesweeper");
+
+        statusbar = new JLabel("");
+        add(statusbar, BorderLayout.SOUTH);
+
+        add(new Board(statusbar));
+
+        setResizable(false);
+    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("http://zetcode.com/tutorials/javagamestutorial/minesweeper/");
+		SwingUtilities.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() {                
+                JFrame ex = new Mines();
+                ex.setVisible(true);                
+            }
+        });
 	}
 
 }
